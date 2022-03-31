6a0daa5a4e3896ec77fce845db34e7ac6e58110a
Nicole Mastrodomenico <nicole.mastrodomenico@axios.com>
2022-03-30 22:38:41 -0400

    gradient background on NewGame grid

--- web/src/components/NewGame.js
+++ web/src/components/NewGame.js
@@ -1,34 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { Grid, Input, Button, Segment } from 'semantic-ui-react'
+import { Grid, Input, Button, Segment } from "semantic-ui-react";
 
 import { createGame } from "../firebase";
 import { Spinner } from "./Spinner";
 import { ErrorOverlay } from "./ErrorOverlay";
 
-const NewGame = ({token}) => {
+const NewGame = ({ token }) => {
   const navigate = useNavigate();
-  const [ error, setError ] = useState(null);
-  const [ isLoading, setIsLoading ] = useState(false);
-  const [ roomId, setRoomId ] = useState('');
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [roomId, setRoomId] = useState("");
 
   const addGame = async () => {
     setIsLoading(true);
 
     try {
-      const { data: { id: gameId } } = await createGame({ token });
+      const {
+        data: { id: gameId },
+      } = await createGame({ token });
       navigate(`/game/${gameId}/`);
     } catch (error) {
-      setError({code: error.code, message: error.message});
+      setError({ code: error.code, message: error.message });
     }
 
     setIsLoading(false);
-  }
+  };
 
   const joinGame = async () => {
     navigate(`/game/${roomId}/`);
-  }
+  };
 
   if (error) {
     return <ErrorOverlay message="Failed to Create New Game" />;
@@ -39,23 +41,40 @@ const NewGame = ({token}) => {
   }
 
   return (
-    <Grid container  style={{minHeight: 'calc(100vh)'}}>
-      <Grid.Row centered>
-        <Grid.Column verticalAlign='middle' padded width={8}>
-          <Segment vertical basic>
-            <Input fluid size="huge" placeholder='Room ID' value={roomId} onChange={e => setRoomId(e.target.value)}/>
-          </Segment>
-          <Segment vertical basic>
-            <Button.Group size="huge" widths="8">
-              <Button primary onClick={addGame}>Create</Button>
-              <Button.Or />
-              <Button secondary disabled={!roomId} onClick={joinGame}>Join</Button>
-            </Button.Group>
-          </Segment>
-        </Grid.Column>
-      </Grid.Row>
+    <Grid
+      style={{
+        background:
+          "linear-gradient(217deg, rgba(255,0,0,.8), rgba(255,0,0,0) 70.71%), linear-gradient(127deg, rgba(0, 0, 255, .8), rgba(0, 0, 255,0) 70.71%), linear-gradient(180deg, rgba(0, 0, 0, 0), rgba(30, 5, 61, 100) 90%)",
+      }}
+    >
+      <Grid container style={{ minHeight: "calc(100vh)" }}>
+        <Grid.Row centered>
+          <Grid.Column verticalAlign="middle" padded width={8}>
+            <Segment vertical basic>
+              <Input
+                fluid
+                size="huge"
+                placeholder="Room ID"
+                value={roomId}
+                onChange={(e) => setRoomId(e.target.value)}
+              />
+            </Segment>
+            <Segment vertical basic>
+              <Button.Group size="huge" widths="8">
+                <Button primary onClick={addGame}>
+                  Create
+                </Button>
+                <Button.Or />
+                <Button secondary disabled={!roomId} onClick={joinGame}>
+                  Join
+                </Button>
+              </Button.Group>
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </Grid>
   );
-}
+};
 
 export default NewGame;
