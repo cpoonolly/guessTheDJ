f3af4210bfcbfa97bc9afca6a9709e886ed79621
nicole026 <69874635+nicole026@users.noreply.github.com>
2022-03-30 23:13:50 -0400

    Nicole/home new game design (#8)
    * gradient background on NewGame grid
    
    * backgorund on App, button color set to purple, custom class added

--- web/src/App.js
+++ web/src/App.js
@@ -1,35 +1,49 @@
-import React from 'react';
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import './App.css';
-import NewGame from './components/NewGame';
-import TodaysGame from './components/TodaysGame';
-import PastGame from './components/PastGame';
-import MySongs from './components/MySongs';
-import { useUser } from './hooks/useUser';
+import "./App.css";
+import NewGame from "./components/NewGame";
+import TodaysGame from "./components/TodaysGame";
+import PastGame from "./components/PastGame";
+import MySongs from "./components/MySongs";
+import { useUser } from "./hooks/useUser";
 
 const App = () => {
-  const {user, token, error} = useUser();
+  const { user, token, error } = useUser();
 
   if (error) {
     console.error(error);
-    return (<div>Failed to Login</div>);
+    return <div>Failed to Login</div>;
   }
 
   if (!user || !token) {
-    return (<div>Loading User...</div>);
+    return <div>Loading User...</div>;
   }
 
   return (
-    <div>
+    <div
+      style={{
+        background:
+          "linear-gradient(217deg, rgba(255,0,0,.8), rgba(255,0,0,0) 70.71%), linear-gradient(127deg, rgba(0, 0, 255, .8), rgba(0, 0, 255,0) 70.71%), linear-gradient(180deg, rgba(0, 0, 0, 0), rgba(30, 5, 61, 100) 90%)",
+      }}
+    >
       <Routes>
         <Route path="/" element={<NewGame user={user} token={token} />} />
-        <Route path="/game/:gameId/" element={<TodaysGame user={user} token={token} />} />
-        <Route path="/game/:gameId/date/:date" element={<PastGame user={user} token={token} />} />
-        <Route path="/game/:gameId/songs" element={<MySongs user={user} token={token} />} />
+        <Route
+          path="/game/:gameId/"
+          element={<TodaysGame user={user} token={token} />}
+        />
+        <Route
+          path="/game/:gameId/date/:date"
+          element={<PastGame user={user} token={token} />}
+        />
+        <Route
+          path="/game/:gameId/songs"
+          element={<MySongs user={user} token={token} />}
+        />
       </Routes>
     </div>
   );
-}
+};
 
 export default App;
--- web/src/components/NewGame.js
+++ web/src/components/NewGame.js
@@ -1,34 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { Grid, Input, Button, Segment } from 'semantic-ui-react'
+import { Grid, Input, Button, Segment } from "semantic-ui-react";
 
 import { createGame } from "../firebase";
 import { Spinner } from "./Spinner";
 import { ErrorOverlay } from "./ErrorOverlay";
 
-const NewGame = ({token}) => {
+const NewGame = ({ token }) => {
   const navigate = useNavigate();
-  const [ error, setError ] = useState(null);
-  const [ isLoading, setIsLoading ] = useState(false);
-  const [ roomId, setRoomId ] = useState('');
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [roomId, setRoomId] = useState("");
 
   const addGame = async () => {
     setIsLoading(true);
 
     try {
-      const { data: { id: gameId } } = await createGame({ token });
+      const {
+        data: { id: gameId },
+      } = await createGame({ token });
       navigate(`/game/${gameId}/`);
     } catch (error) {
-      setError({code: error.code, message: error.message});
+      setError({ code: error.code, message: error.message });
     }
 
     setIsLoading(false);
-  }
+  };
 
   const joinGame = async () => {
     navigate(`/game/${roomId}/`);
-  }
+  };
 
   if (error) {
     return <ErrorOverlay message="Failed to Create New Game" />;
@@ -39,23 +41,33 @@ const NewGame = ({token}) => {
   }
 
   return (
-    <Grid container  style={{minHeight: 'calc(100vh)'}}>
+    <Grid container style={{ minHeight: "calc(100vh)" }}>
       <Grid.Row centered>
-        <Grid.Column verticalAlign='middle' padded width={8}>
+        <Grid.Column verticalAlign="middle" padded width={8}>
           <Segment vertical basic>
-            <Input fluid size="huge" placeholder='Room ID' value={roomId} onChange={e => setRoomId(e.target.value)}/>
+            <Input
+              fluid
+              size="huge"
+              placeholder="Room ID"
+              value={roomId}
+              onChange={(e) => setRoomId(e.target.value)}
+            />
           </Segment>
           <Segment vertical basic>
             <Button.Group size="huge" widths="8">
-              <Button primary onClick={addGame}>Create</Button>
+              <Button className="button-primary" onClick={addGame}>
+                Create
+              </Button>
               <Button.Or />
-              <Button secondary disabled={!roomId} onClick={joinGame}>Join</Button>
+              <Button color="black" disabled={!roomId} onClick={joinGame}>
+                Join
+              </Button>
             </Button.Group>
           </Segment>
         </Grid.Column>
       </Grid.Row>
     </Grid>
   );
-}
+};
 
 export default NewGame;
