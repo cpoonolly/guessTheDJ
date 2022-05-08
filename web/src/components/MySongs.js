f30b137196118aedac40ebdf6db2dbaacb4807ad
Nicole Mastrodomenico <nicole.mastrodomenico@axios.com>
2022-05-07 22:41:17 -0400

    little styling on my songs page, wip

--- web/src/components/MySongs.js
+++ web/src/components/MySongs.js
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 
-import { useGame } from '../hooks/useGame';
+import { useGame } from "../hooks/useGame";
 import { addSong, removeSong, addVote } from "../firebase";
 import { Spinner } from "./Spinner";
 import { ErrorOverlay } from "./ErrorOverlay";
 import { ErrorMessage } from "./ErrorMessage";
+import { Container, Grid, Label, Header, Segment } from "semantic-ui-react";
 
 const MySongs = ({ token }) => {
   const { gameId } = useParams();
   const { game, error, refresh: refreshGame } = useGame({ token, gameId });
-  const [ songUrl, setSongUrl ] = useState(null);
-  const [ isLoading, setIsLoading ] = useState(false);
-  const [ errorBanner, setErrorBanner ] = useState(null);
+  const [songUrl, setSongUrl] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorBanner, setErrorBanner] = useState(null);
 
   const handleSongAdd = async () => {
     setIsLoading(true);
@@ -22,7 +23,7 @@ const MySongs = ({ token }) => {
       await refreshGame();
       setSongUrl(null);
     } catch (error) {
-      setErrorBanner('Failed to add Song');
+      setErrorBanner("Failed to add Song");
     }
 
     setIsLoading(false);
@@ -35,9 +36,9 @@ const MySongs = ({ token }) => {
       await removeSong({ token, gameId, songId });
       await refreshGame();
     } catch (error) {
-      setErrorBanner('Failed to remove Song');
+      setErrorBanner("Failed to remove Song");
     }
-    
+
     setIsLoading(false);
   };
 
@@ -48,7 +49,7 @@ const MySongs = ({ token }) => {
       await addVote({ token, gameId, vote: userId });
       await refreshGame();
     } catch (error) {
-      setErrorBanner('Failed to add Vote');
+      setErrorBanner("Failed to add Vote");
     }
     setIsLoading(false);
   };
@@ -61,66 +62,106 @@ const MySongs = ({ token }) => {
     return <Spinner />;
   }
 
-  const { vote, users, playedSongs, unplayedSongs, daysSong: todaysSong } = game;
+  const {
+    vote,
+    users,
+    playedSongs,
+    unplayedSongs,
+    daysSong: todaysSong,
+  } = game;
 
   return (
     <>
       {isLoading ? <Spinner /> : <></>}
-      {errorBanner ? <ErrorMessage message="Something went wrong" subMessage={errorBanner} onDismiss={() => setErrorBanner(null)} /> : <></>}
+      {errorBanner ? (
+        <ErrorMessage
+          message="Something went wrong"
+          subMessage={errorBanner}
+          onDismiss={() => setErrorBanner(null)}
+        />
+      ) : (
+        <></>
+      )}
       {todaysSong ? (
         <>
           <div>
-            <p><strong>Today's Song:</strong></p>
+            <p>
+              <strong>Today's Song:</strong>
+            </p>
             <p>{JSON.stringify(todaysSong)}</p>
           </div>
           <div>
-            <p><strong>Current Vote:</strong></p>
-            <p>{vote}</p> 
+            <p>
+              <strong>Current Vote:</strong>
+            </p>
+            <p>{vote}</p>
           </div>
         </>
       ) : (
         <></>
       )}
-      <div>
-        <p><strong>Users:</strong></p>
-        <ul>
-          {users.map(user => (
-            <li key={user.id}>
+
+      <Container>
+        <Header>DJs:</Header>
+        {/* <ul> */}
+        <Grid stackable columns={2}>
+          {users.map((user) => (
+            <Grid.Column key={user.id}>
               {todaysSong ? (
-                <a onClick={() => handleVoteAdd(user.id)} href="javascript:void(0)">(Vote)</a>
+                <a
+                  onClick={() => handleVoteAdd(user.id)}
+                  href="javascript:void(0)"
+                >
+                  (Vote)
+                </a>
               ) : (
                 <></>
               )}
-              {JSON.stringify(user)}
-            </li>
+              {/* {JSON.stringify(user)} */}
+              <Label>{user.name}</Label>
+            </Grid.Column>
           ))}
-        </ul>
-      </div>
+        </Grid>
+        {/* </ul> */}
+      </Container>
       <div>
-        <p><strong>Unplayed Songs:</strong></p>
+        <p>
+          <strong>Unplayed Songs:</strong>
+        </p>
         <ul>
-          {unplayedSongs.map(song => (
+          {unplayedSongs.map((song) => (
             <li key={song.id}>
-              <a onClick={() => handleSongRemove(song.id)} href="javascript:void(0)">(Delete)</a>
+              <a
+                onClick={() => handleSongRemove(song.id)}
+                href="javascript:void(0)"
+              >
+                (Delete)
+              </a>
               {JSON.stringify(song)}
             </li>
           ))}
         </ul>
       </div>
       <div>
-        <input type={'text'} value={songUrl} onChange={e => setSongUrl(e.target.value)} />
+        <input
+          type={"text"}
+          value={songUrl}
+          onChange={(e) => setSongUrl(e.target.value)}
+        />
         <button onClick={handleSongAdd}>Add Song</button>
       </div>
       <div>
-        <p><strong>Played Songs:</strong></p>
+        <p>
+          <strong>Played Songs:</strong>
+        </p>
         <ul>
-          {playedSongs.map(song => (
+          {playedSongs.map((song) => (
             <li key={song.id}>{JSON.stringify(song)}</li>
           ))}
         </ul>
       </div>
     </>
   );
-}
+};
 
 export default MySongs;
